31c4039ded59ec33b17b960f8330ad465cc7f87f
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-07-13 08:20:43 +0100

    added file to install nginx with puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,37 @@
+# Installing and configuring NGINX on Ubuntu server
+
+# update system repo
+exec { 'init_commands':
+  command  => 'sudo apt-get -y update',
+  provider => shell,
+  path     => ['/bin', '/usr/bin', '/usr/sbin'],
+}
+
+# install nginx
+class{'nginx':
+    manage_repo    => true,
+    package_source => 'nginx-mainline'
+}
+
+# 'Hello World!' default response configuration
+file {'default_reponse':
+    ensure  => 'present',
+    path    => '/var/www/html/index.html',
+    content => 'Hello World!',
+    require => Class['nginx']
+}
+
+# 301 redirection configuration
+file_line {'redirect_conf':
+    ensure => 'present',
+    path   => '/etc/nginx/sites-available/default',
+    after  => 'server_name _',
+    line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+# restart nginx to impliment configuration
+exec {'start_nginx':
+    command  => 'sudo service nginx start',
+    provider => shell,
+    path     => ['/bin'/, '/usr/bin', '/usr/sbin'],
+}

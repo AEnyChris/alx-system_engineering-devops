e0263f9564b18e8092e6a4d855bca4784781a7e2
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-07-15 01:25:16 +0100

    puppet installation of haproxy

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,45 @@
+# Installing and configuring NGINX on Ubuntu server
+
+# update system repo
+exec { 'init_commands':
+  command  => 'sudo apt-get -y update',
+  provider => shell,
+  path     => ['/bin', '/usr/bin', '/usr/sbin'],
+}
+
+# install nginx
+package {'nginx':
+  ensure => 'installed'
+}
+
+# 'Hello World!' default response configuration
+file {'default_reponse':
+    ensure  => 'present',
+    path    => '/var/www/html/index.html',
+    content => 'Hello World!',
+}
+
+# 301 redirection configuration
+file_line {'redirect_conf':
+    ensure => 'present',
+    path   => '/etc/nginx/sites-available/default',
+    after  => 'server_name _',
+    line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+# Adding custom header to nginx
+file_line {'add_header':
+    ensure => 'present',
+    path   => '/etc/nginx/sites-available/default',
+    after  => 'root /var/www/html;',
+    line   => 'add_header X-Served-By $hostname;',
+}
+
+
+
+# restart nginx to impliment configuration
+exec {'start_nginx':
+    command  => 'sudo service nginx start',
+    provider => shell,
+    path     => ['/bin', '/usr/bin', '/usr/sbin'],
+}

be51288a6e24525d526c63cf2b26f31f42055123
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-07-30 01:34:52 +0100

    extracting data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,31 +5,33 @@ for a given employee ID, returns information
 about his/her TODO list progress.
 """
 
-from sys import argv
-import requests
+
+if __name__ == "__main__":
+    from sys import argv
+    import requests
 
 
-# Extract data from API
-user_id = argv[1]
-user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/"
-todo_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos/"
-user_json = requests.get(user_url)
-todo_json = requests.get(todo_url)
+    # Extract data from API
+    user_id = argv[1]
+    user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/"
+    todo_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos/"
+    user_json = requests.get(user_url)
+    todo_json = requests.get(todo_url)
 
-# Convert JSON data to Python objects
-user_data = user_json.json()
-todo_list = todo_json.json()
+    # Convert JSON data to Python objects
+    user_data = user_json.json()
+    todo_list = todo_json.json()
 
-# Parse data to get information
-empname = user_data["name"]
-task_done = [task for task in todo_list if task["completed"] is True]
-no_task_done = len(task_done)
-total_tasks = len(todo_list)
+    # Parse data to get information
+    empname = user_data.get("name")
+    task_done = [task for task in todo_list if task.get("completed") is True]
+    no_task_done = len(task_done)
+    total_tasks = len(todo_list)
 
-# Information outuput
-print(
-        f"Employee {empname} is done with tasks({no_task_done}/{total_tasks})"
-     )
+    # Information outuput
+    print(
+            f"Employee {empname} is done with tasks({no_task_done}/{total_tasks})"
+        )
 
-for task in task_done:
-    print(f"\t {task['title']}")
+    for task in task_done:
+        print(f"\t {task['title']}")

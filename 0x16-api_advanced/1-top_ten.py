3cdfd654aefd96c6fc4bb8348552676cb453a8e8
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-08-14 00:43:39 +0100

    get top ten hottest posts

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,14 +6,11 @@ import requests
 
 def top_ten(subreddit):
     """function returns the title of top ten hot posts"""
-    try:
-        url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-        headers = {"User-Agent": "MyApp/1.0"}
-        payload = requests.get(url, allow_redirects=False, headers=headers)
-        if payload.status_code == 200:
-            for item in payload.json()['data']['children'][:10]:
-                print(item['data']['title'])
-        else:
-            print(None)
-    except Exception as e:
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "MyApp/1.0"}
+    payload = requests.get(url, allow_redirects=False, headers=headers)
+    if payload.status_code == 200:
+        for item in payload.json()['data']['children'][:10]:
+            print(item['data']['title'])
+    else:
         print(None)

4cb047a214cf5c185a37930d4726f1967ee298d4
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-08-13 23:56:53 +0100

    added header

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,8 @@ def number_of_subscribers(subreddit):
     """function returns the number of subs for a subreddit"""
     try:
         url = f"https://www.reddit.com/r/{subreddit}/about.json"
-        payload = requests.get(url, allow_redirects=False)
+        headers = {"User-Agent": "MyApp/1.0"}
+        payload = requests.get(url, allow_redirects=False, headers=headers)
         if payload.status_code != 200:
             return 0
         subs = payload.json()['data']['subscribers']

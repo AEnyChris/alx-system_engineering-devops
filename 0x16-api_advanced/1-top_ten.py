b9565fddae4dd556facb4578fd692e00b0a058ec
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-08-14 01:48:48 +0100

    updated

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ import requests
 def top_ten(subreddit):
     """function returns the title of top ten hot posts"""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {"User-Agent": "MyApp/1.0"}
+    headers = {"User-Agent": "My-User-Agent"}
     payload = requests.get(url, allow_redirects=False, headers=headers)
     if payload.status_code == 200:
         for item in payload.json()['data']['children'][:10]:
